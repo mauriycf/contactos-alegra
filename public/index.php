3e05e178599b8d1c6b9d4ac70699fe6928b68837
mauriycf <mauriycf@gmail.com>
2017-04-27 17:07:37 -0400

    kkk

--- public/index.php
+++ public/index.php
@@ -24,7 +24,7 @@ require_once 'Zend/Application.php';
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
+    APPLICATION_PATH . '/application/configs/application.ini'
 );
 $application->bootstrap()
 <<<<<<< HEAD
